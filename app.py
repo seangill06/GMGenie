a3eff21ef5deabccb268c402b2600fbd7a49ed1c
Sean Gill <seangill06@gmail.com>
2025-08-18 00:05:56 -0700

    Re-add app to root folder

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="GM Genie",
+    page_icon="🔮"
+)
+
+st.title("Welcome to GM Genie!")
+st.write("Please select a tool from the navigation sidebar.")
